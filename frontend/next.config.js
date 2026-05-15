c956d973a819f4571986f49c16d0986042adfd5e
Rahat Ahmed <87570918+Rahat0764@users.noreply.github.com>
2026-05-15 18:16:23 +0600

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,21 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [],
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'drive.google.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'predict.rahatx.workers.dev' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
     ]
   }
-}
-module.exports = nextConfig
+};
+
+module.exports = withPWA(nextConfig);
